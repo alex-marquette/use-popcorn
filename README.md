6ec7d72e7e971d62c90383893ad738a65cce5c32
alex-marquette <35579617+alex-marquette@users.noreply.github.com>
2024-07-25 11:06:33 -0500

    creating a reusable star component and updated ReadMe file

---
+++ README.md
@@ -0,0 +1,7 @@
+#Use-Popcorn
+
+##Description
+This was a simple react project built within the Udemy Course "The Ultimate React Course 2024: React, Next.js, Redux & More" by Jonas Schmedtmann
+Course Link: https://www.udemy.com/course/the-ultimate-react-course/learn/lecture/37350750#overview. 
+
+The app is setup to teach students in the course about components, composition, and reusability. The section also details prop drilling and how to structure apps to avoid nested prop drilling. 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+//import './index.css';
+//import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="#00f" maxRating={10} onSetRating={setMovieRating} />
+      <p> this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App/> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} color="#ff0000" size={24} />
+
+    <Test />
   </React.StrictMode>
 );
-
